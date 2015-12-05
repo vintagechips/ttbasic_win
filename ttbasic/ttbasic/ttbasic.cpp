067b5d23998d4513901fc841c0e4c585d787d18e
vintagechips <vintagechips@live.jp>
2015-12-05 12:46:31 +0900

    fixed
    final version

---
+++ ttbasic/ttbasic/ttbasic.cpp
@@ -0,0 +1,14 @@
+// basic.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <stdlib.h> // for srand
+#include <time.h> // for time
+
+void basic(void);// prototype
+
+int main() {
+	srand((unsigned int)time(0)); // for RND function
+	basic(); // call The BASIC
+	return 0;
+}
